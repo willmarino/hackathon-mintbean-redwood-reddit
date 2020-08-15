365959e7d2873cca8972b52f8cb19f0db003e711
Will <w.marino997@gmail.com>
2020-08-15 14:47:34 -0400

    auth setup

--- web/src/layouts/NavLayout/NavLayout.js
+++ web/src/layouts/NavLayout/NavLayout.js
@@ -1,8 +1,14 @@
+import { useAuth } from '@redwoodjs/auth';
+
 const NavLayout = ({ children }) => {
+  const { logIn, isAuthenticated, logOut, currentUser } = useAuth();
+  const display = (isAuthenticated) ? <p>{currentUser.email}</p> : null;
+
   return <>
     <div className="navbar">
       <div>Redwood-reddit-clone</div>
-      <div>Login</div>
+      {display}
+      <a href="#" onClick={ isAuthenticated ? logOut : logIn}>{ isAuthenticated ? 'Log Out' : "Log In" }</a>
     </div>
     <main>{children}</main>
   </>
