79cd604ab32ad87d4c03b935ee1d4c05478c946a
Will <w.marino997@gmail.com>
2020-08-15 14:16:35 -0400

    navbar layout, some styling

--- web/src/pages/MainPage/MainPage.js
+++ web/src/pages/MainPage/MainPage.js
@@ -1,14 +1,17 @@
 import { Link } from '@redwoodjs/router'
+import NavLayout from 'src/layouts/NavLayout/NavLayout';
 
 const MainPage = () => {
   return (
     <>
-      <h1>MainPage</h1>
-      <p>Find me in "./web/src/pages/MainPage/MainPage.js"</p>
-      <p>
-        My default route is named "main", link to me with `
-        <Link to="main">routes.main()</Link>`
-      </p>
+      <NavLayout>
+        <h1>MainPage</h1>
+        <p>Find me in "./web/src/pages/MainPage/MainPage.js"</p>
+        <p>
+          My default route is named "main", link to me with `
+          <Link to="main">routes.main()</Link>`
+        </p>
+      </NavLayout>
     </>
   )
 }
